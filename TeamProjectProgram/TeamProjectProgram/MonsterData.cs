86ec385deafb38c9a08ad981a5ef67af3419bd7e
bichen <alj2664@gmail.com>
2026-08-27 14:39:30 +0900

    몬스터데이터 레벨 추가

--- TeamProjectProgram/TeamProjectProgram/MonsterData.cs
+++ TeamProjectProgram/TeamProjectProgram/MonsterData.cs
@@ -4,10 +4,13 @@ public struct MonsterData
     public string Name;
     public int Health;
 
-    public MonsterData(string name, int health)
+    public int Level;
+
+    public MonsterData(string name, int health, int Level)
     {
         Name = name;
         Health = health;
+        Level = Level;
     }
 }
 
