38093b64d8fdaf83057e7c6c65f04d952bcb43be
bichen <alj2664@gmail.com>
2026-08-27 14:36:09 +0900

    몬스터 데이터

---
+++ TeamProjectProgram/TeamProjectProgram/MonsterData.cs
@@ -0,0 +1,13 @@
+﻿
+public struct MonsterData
+{
+    public string Name;
+    public int Health;
+
+    public MonsterData(string name, int health)
+    {
+        Name = name;
+        Health = health;
+    }
+}
+
