9b7bb4350e9f5ccefff28a7403be7171c516247f
SquashPrince <wowns1450@naver.com>
2026-08-27 14:36:01 +0900

    [Feat] Monster 생성자 정의,  Slime => IDamageable 상속

--- TeamProjectProgram/TeamProjectProgram/Monster.cs
+++ TeamProjectProgram/TeamProjectProgram/Monster.cs
@@ -1,5 +1,21 @@
 ﻿using System;
 public abstract class Monster
 {
-    public string Name { get; set; } = "";
+    private string _name;
+    public string Name
+    {
+        get
+        { 
+            return _name;
+        }
+        set
+        { 
+            _name = value;
+        }
+    }
+
+    public Monster(string name)
+    {
+        Name = name;
+    }
 }
